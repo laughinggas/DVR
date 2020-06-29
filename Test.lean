a9adafeb99e11f23647f5b967cd049b8be87d3c1
laughinggas <58670661+laughinggas@users.noreply.github.com>
2020-06-29 12:55:43 +0100

    Add files via upload

---
+++ Test.lean
@@ -0,0 +1,495 @@
+import ring_theory.ideals
+
+import ring_theory.principal_ideal_domain
+
+import ring_theory.localization
+
+import tactic
+
+universe u
+
+/-lemma val (a b c : ℤ ) (h : a*b = c) (f : ℤ -> ℤ) : f(a*b) = f(c) :=
+begin
+  rw h,
+end -/
+
+class discrete_valuation_ring (R : Type u) [integral_domain R] [is_principal_ideal_ring R] :=
+
+(prime_ideal' : ideal R)
+
+(primality : prime_ideal'.is_prime)
+
+(is_nonzero : prime_ideal' ≠ ⊥)
+
+(unique_nonzero_prime_ideal : ∀ P : ideal R, P.is_prime → P = ⊥ ∨ P = prime_ideal')
+
+namespace discrete_valuation_ring
+
+def prime_ideal (R : Type u) [integral_domain R] [is_principal_ideal_ring R] [discrete_valuation_ring R] : ideal R :=
+discrete_valuation_ring.prime_ideal'
+
+instance is_prime (R : Type*) [integral_domain R] [is_principal_ideal_ring R] [discrete_valuation_ring R] : (prime_ideal R).is_prime :=
+primality
+
+variables {R : Type u} [integral_domain R] [is_principal_ideal_ring R] [discrete_valuation_ring R]
+open discrete_valuation_ring
+
+lemma prime_ideal_is_maximal : (prime_ideal R).is_maximal :=
+begin
+  have f : prime_ideal R ≠ ⊥,
+  { apply discrete_valuation_ring.is_nonzero },
+  apply is_prime.to_maximal_ideal,
+    exact f,
+end
+
+lemma unique_max_ideal : ∃! I : ideal R, I.is_maximal :=
+
+begin
+
+  use prime_ideal R,
+
+  split,
+
+  { exact prime_ideal_is_maximal },
+
+  { intros y a,
+
+    cases discrete_valuation_ring.unique_nonzero_prime_ideal y a.is_prime,
+
+    { exfalso,
+
+      rw h at a,
+
+      apply discrete_valuation_ring.primality.left,
+
+      exact a.right (prime_ideal R) (bot_lt_iff_ne_bot.2 discrete_valuation_ring.is_nonzero) },
+
+    { assumption } }
+
+end
+
+instance is_local_ring : local_ring R := local_of_unique_max_ideal unique_max_ideal
+
+open local_ring
+
+noncomputable theory
+open_locale classical
+class discrete_valuation_field (K : Type*) [field K] :=
+(v : K -> with_top ℤ ) 
+(mul : ∀ (x y : K), v(x*y) = v(x) + v(y) )
+(add : ∀ (x y : K), min (v(x)) (v(y)) ≤ v(x + y)  )
+(non_zero : ∀ (x : K), v(x) = ⊤ ↔ x = 0 )
+
+namespace discrete_valuation_field
+
+definition valuation (K : Type*) [field K] [ discrete_valuation_field K ] : K -> with_top ℤ := v
+
+variables {K : Type*} [field K] [discrete_valuation_field K]
+
+lemma with_top.cases (a : with_top ℤ) : a = ⊤ ∨ ∃ n : ℤ, a = n :=
+begin
+  cases a with n,
+  { -- a = ⊤ case
+    left,
+    refl, -- true by definition
+  },
+  { -- ℤ case
+    right,
+    use n,
+    refl, -- true by definition
+  }
+end
+
+lemma sum_zero_iff_zero (a : with_top ℤ) : a + a = 0 ↔ a = 0 :=
+begin
+  split,
+  { -- the hard way
+    intro h, -- h is a proof of a+a=0
+    -- split into cases
+    cases (with_top.cases a) with htop hn,
+    { -- a = ⊤
+      rw htop at h,
+      -- h is false
+      cases h,
+      -- no cases!
+    },
+    { -- a = n
+      cases hn with n hn,
+      rw hn at h ⊢,
+      -- now h says n+n=0 and our goal is n=0
+      -- but these are equalities in `with_top ℤ
+      -- so we need to get them into ℤ
+      -- A tactic called `norm_cast` does this
+     norm_cast at h ⊢,
+      -- we finally have a hypothesis n + n = 0
+      -- and a goal n = 0
+      -- and everything is an integer
+      rw add_self_eq_zero at h,
+      assumption
+    }
+  },
+   { -- the easy way
+    intro ha,
+    rw ha,
+    simp
+  }
+end
+ --Thanks Kevin!
+
+lemma val_one_eq_zero : v(1 : K) = 0 :=
+begin
+have f : (1:K)*(1:K) = (1 : K),
+simp,
+have g : v(1 : K) = v((1 : K)*(1 : K)),
+simp,
+have k : v((1 : K)*(1 : K)) = v(1 : K) + v(1 : K),
+{apply mul},
+rw k at g,
+rw g,
+cases (with_top.cases (v(1:K))) with h1 h2,
+{
+rw h1,
+rw h1 at g,
+rw <-g,
+rw non_zero at h1,
+exfalso,
+exact one_ne_zero h1,
+},
+cases h2 with n h2,
+{
+rw h2,
+rw sum_zero_iff_zero,
+rw h2 at g,
+norm_cast at g,
+simp,
+linarith,
+},
+end
+
+lemma val_minus_one_is_zero : v(-1 : K) = 0 :=
+begin
+have f : (-1:K)*(-1:K) = (1 : K),
+simp,
+have g : v((-1 : K)*(-1 : K)) = v(1 : K),
+simp,
+have k : v((-1 : K)*(-1 : K)) = v(-1 : K) + v(-1 : K),
+{
+  apply mul,
+},
+rw k at g,
+rw val_one_eq_zero at g,
+rw <-sum_zero_iff_zero,
+exact g,
+end
+
+lemma val_zero : v(0:K) = ⊤ :=
+begin
+rw non_zero,
+end
+
+
+lemma with_top.transitivity (a b c : with_top ℤ ) : a ≤ b -> b ≤ c -> a ≤ c :=
+begin
+rintros,
+cases(with_top.cases c) with h1 h2,
+  {
+    rw h1,
+    simp,
+  },
+  {
+    cases h2 with n h2,
+    cases(with_top.cases a) with k1 k2,
+    {
+      rw [k1, h2],
+      rw k1 at a_1,
+      rw h2 at a_2,
+      cases(with_top.cases b) with l1 l2,
+      {
+        rw l1 at a_2,
+        exact a_2,
+      },
+      {
+        cases l2 with m l2,
+        rw l2 at a_1,
+        exfalso,
+        apply with_top.not_top_le_coe m,
+        exact a_1,
+      },
+    },
+    {
+      cases k2 with m k2,
+      cases(with_top.cases b) with l1 l2,
+      {
+        rw [l1,h2] at a_2,
+        exfalso,
+        apply with_top.not_top_le_coe n,
+        exact a_2,
+      },
+      {
+        cases l2 with k l2,
+        rw [k2,l2] at a_1,
+        rw [l2,h2] at a_2,
+        rw [k2,h2],
+        rw with_top.coe_le_coe,
+        rw with_top.coe_le_coe at a_1,
+        rw with_top.coe_le_coe at a_2,
+        transitivity k,
+        exact a_1,
+        exact a_2,
+      },
+    },  
+  },
+end 
+
+def val_ring (K : Type*) [field K] [discrete_valuation_field K] := { x : K | 0 ≤ v x } 
+
+instance (K : Type*) [field K] [discrete_valuation_field K] : is_add_subgroup (val_ring K) :=
+{
+  zero_mem := begin
+              unfold val_ring,
+              simp,
+              rw val_zero,
+              simp,  
+              end,
+  add_mem := begin
+            unfold val_ring,
+            simp,
+            rintros,
+            have g : min (v(a)) (v(b)) ≤ v(a + b),
+            {
+              apply add,
+            },
+            rw min_le_iff at g,
+            cases g,
+            {
+              apply with_top.transitivity,
+              exact a_1,
+              exact g,
+            },
+            {
+              apply with_top.transitivity,
+              exact a_2,
+              exact g,
+            },
+            end,
+  neg_mem := begin
+            unfold val_ring,
+            rintros,
+            simp,
+            simp at a_1,
+            have f : -a = a * (-1 : K),
+            {
+              simp,
+            },
+            rw f,
+            rw mul,
+            rw val_minus_one_is_zero,
+            simp,
+            assumption, 
+            end,
+}
+
+instance (K:Type*) [field K] [discrete_valuation_field K] : is_submonoid (val_ring K) :=
+{ one_mem := begin
+            unfold val_ring,
+            simp,
+            rw val_one_eq_zero,
+            norm_num,
+            end,
+  mul_mem := begin
+            unfold val_ring,
+            rintros,
+            simp,
+            simp at a_1,
+            simp at a_2,
+            rw mul,
+            apply add_le_add' a_1 a_2,
+            end, }
+
+instance valuation_ring (K:Type*) [field K] [discrete_valuation_field K] : is_subring (val_ring K) :=
+begin
+refine is_subring.mk,
+end
+
+instance is_domain (K:Type*) [field K] [discrete_valuation_field K] : integral_domain (val_ring K) :=
+begin
+apply subring.domain (val_ring K),
+end
+
+lemma unit_iff_val_zero (K:Type*) [field K] [discrete_valuation_field K] [α : (val_ring K)] : v (α : K) = 0 ↔ is_unit α := 
+begin
+split,
+{
+  rintros,
+  rw is_unit_iff_exists_inv,
+  use α⁻¹,
+  {
+    unfold val_ring,
+    simp,
+    have f : v((α : K) * (α⁻¹ : K)) = 0,
+    {
+      rw mul_inv_cancel,
+      {
+        rw val_one_eq_zero,
+      },
+      {
+        from λ h,
+        by 
+        {
+          rw [<-non_zero, a] at h,
+          cases h,
+        },
+      },
+    },
+  rw mul at f,
+  rw a at f,
+  simp at f,
+  rw f,
+  norm_num,  
+  },
+  {
+    sorry,
+  },
+},
+{
+  rintros,
+  rw is_unit_iff_exists_inv at a,
+  cases a with b a,
+  have f : v((α:K)*(b:K)) = v(1:K),
+  {
+    norm_cast,
+    rw a,
+    simp,
+  },
+  rw mul at f,
+  rw val_one_eq_zero at f,
+  rw add_eq_zero_iff' at f,
+  {
+    cases f,
+    exact f_left,
+  },
+  {
+    exact α.2,
+  },
+  {
+    exact b.2,
+  },
+}
+end
+
+def unif (K:Type*) [field K] [discrete_valuation_field K] : set (val_ring K) := { π | v(π:K) = 1 } 
+
+variables (π : Type*) (π ∈ unif)
+
+lemma is_pir (K:Type*) [field K] [discrete_valuation_field K] : is_principal_ideal_ring (val_ring K) :=
+begin
+split,
+rintros,
+split,
+simp,
+sorry,
+end
+
+end discrete_valuation_field
+
+end discrete_valuation_ring
+
+/-
+instance (K:Type*) [field K] [discrete_valuation_field K] : discrete_valuation_ring {x : K | 0 ≤ v(x) } := 
+begin
+  apply is_domain, 
+  apply is_pir,
+  prime_ideal' := {sorry,},
+  primality := sorry,
+  is_nonzero := sorry,
+  unique_nonzero_prime_ideal := sorry,
+end
+
+
+def valuation_dvr (K:Type*) [field K] [discrete_valuation_field K] (S : Type*) (h : S = {x : K | val(x) ≥ 0}) : discrete_valuation_ring S := 
+{
+  prime_ideal' := {x : K | val(x) > 0},
+  primality := sorry,
+  is_nonzero := _,
+  unique_nonzero_prime_ideal := _,
+}
+-/
+
+
+/-
+def local_pid_dvr {S : Type u} [is_local : local_ring S] [pid: principal_ideal_domain S] (non_field : local_ring.nonunits_ideal S ≠ ⊥ ) :
+  discrete_valuation_ring S :=
+  { prime_ideal' := (nonunits_ideal S),
+  primality := (nonunits_ideal S).is_prime,
+  is_nonzero := _,
+  unique_nonzero_prime_ideal := _,
+  ..pid}
+
+lemma local_pid_dvr {S : Type u} (is_local : local_ring S) (pid: principal_ideal_domain S) (non_field : local_ring.nonunits_ideal S ≠ ⊥ ) : discrete_valuation_ring S :=
+begin
+use local_ring.nonunits_ideal S,
+ 
+rw zero_mem_nonunits S,
+sorry,
+end
+
+end discrete_valuation_ring
+
+definition discrete_valuation_field.discrete_valuation_ring (K: Type*) [discrete_valuation_field K] : extends integral_domain R := 
+(is_subring R)
+(R = {x : K | (val(x)) ≥ 0})
+
+end discrete_valuation_field
+-/ 
+ /-lemma unit_val_one (S:Type*) (K:Type*) [field K] [discrete_valuation_field K] (h : S = {x : K | val(x) ≥ 0}) (a:S) (val a = 1) : (a⁻¹ : K) : S :=
+begin
+  sorry,
+end -/
+
+
+ /- lemma is_integral_domain (K:Type*) [field K] [discrete_valuation_field K] : integral_domain {x : K | 0 ≤ v(x) } := 
+begin
+  let P := {x : K | v(x) > 0},
+  constructor,
+  rintros,
+    {apply mul_comm},
+    {
+      rintros,
+      rw subtype.ext,
+      rw subtype.ext,
+      have g : v ((a:K)*(b:K)) = v(0:K),
+        {
+          sorry,
+        },
+      rw mul at g,
+      rw val_zero at g,
+      rw with_top.add_eq_top at g,
+      cases g,
+        {rw non_zero at g,
+        left,
+        rw subtype.ext at g,
+        rw <-with_top.coe_eq_zero,
+        unfold_coes at *,
+        },
+          
+      },
+    apply zero_ne_one,
+    simp,
+    
+  have f1 : distrib S,
+    {
+      split,
+      sorry,
+
+    },  
+  have f2 : monoid S,
+    {
+      sorry,
+    },
+  have f3 : add_comm_group S,
+    {
+      sorry,
+    },
+  apply f1,  
+  sorry,
+end -/
