0d3f5f56b3472b09567578774d4f77c862bbbace
Ashvni <ashvni.n@gmail.com>
2020-08-02 16:10:12 +0100

    fix errors

--- src/bundled_subrings_working.lean
+++ src/bundled_subrings_working.lean
@@ -29,7 +29,7 @@ universes u v
 
 open group
 
-variables {R : Type u} {S : Type u} {T : Type u} [ring R] [ring S] [ring T]
+variables {R : Type*} {S : Type*} {T : Type u} [ring R] [ring S] [ring T]
 
 set_option old_structure_cmd true
 
@@ -117,6 +117,9 @@ theorem mul_mem : ∀ {x y : R}, x ∈ s → y ∈ s → x * y ∈ s := s.mul_me
 /-- A subring is closed under addition. -/
 theorem add_mem : ∀ {x y : R}, x ∈ s → y ∈ s → x + y ∈ s := s.add_mem'
 
+/-- A subring is closed under negation. -/
+theorem neg_mem : ∀ {x : R}, x ∈ s → -x ∈ s := s.neg_mem'
+
 /-- Product of a list of elements in a subring is in the subring. -/
 lemma list_prod_mem {l : list R} : (∀x ∈ l, x ∈ s) → l.prod ∈ s :=
 s.to_submonoid.list_prod_mem
@@ -155,7 +158,7 @@ lemma pow_mem {x : R} (hx : x ∈ s) (n : ℕ) : x^n ∈ s := s.to_submonoid.pow
 lemma gsmul_mem {x : R} (hx : x ∈ s) (n : ℤ) :
   n •ℤ x ∈ s := s.to_add_subgroup.gsmul_mem hx n
 
-lemma coe_nat_mem (n : ℤ) : (n : R) ∈ s :=
+lemma coe_int_mem (n : ℤ) : (n : R) ∈ s :=
 by simp only [← gsmul_one, gsmul_mem, one_mem]
 
 /-- A subring of a ring inherits a ring structure -/
@@ -200,6 +203,9 @@ lemma coe_coe (s : subring R) : ↥(s : set R) = s := rfl
 @[simp] lemma mem_to_add_subgroup {s : subring R} {x : R} :
   x ∈ s.to_add_subgroup ↔ x ∈ s := iff.rfl
 @[simp] lemma coe_to_add_subgroup (s : subring R) : (s.to_add_subgroup : set R) = s := rfl
+@[simp] lemma mem_to_add_submonoid {s : subring R} {x : R} :
+  x ∈ s.to_add_subgroup.to_add_submonoid ↔ x ∈ s := iff.rfl
+@[simp] lemma coe_to_add_submonoid (s : subring R) : (s.to_add_subgroup.to_add_submonoid : set R) = s := rfl
 
 /-- The subring `R` of the ring `R`. -/
 instance : has_top (subring R) :=
@@ -288,7 +294,8 @@ variables {cR : Type u} [comm_ring cR]
 def subset_comm_ring (S : subring cR) : comm_ring S :=
 {mul_comm := λ _ _, subtype.eq $ mul_comm _ _, ..subring.to_ring S}
 
-instance subtype.comm_ring [S : subring cR] : comm_ring (subtype S) := S.subset_comm_ring cR
+--check if meaning has been changed, isnt it already a type?
+/- instance subtype_comm_ring [S : subring cR] : comm_ring (ring_hom.set_range (subtype S)) := S.subset_comm_ring -/
 
 instance subring.domain {D : Type*} [integral_domain D] [S : subring D] : integral_domain S :=
 { exists_pair_ne := ⟨0, 1, mt subtype.ext_iff_val.1 zero_ne_one⟩,
@@ -296,26 +303,176 @@ instance subring.domain {D : Type*} [integral_domain D] [S : subring D] : integr
     by { simp only [subtype.ext_iff_val, subtype.coe_mk], exact eq_zero_or_eq_zero_of_mul_eq_zero },
   .. S.subset_comm_ring, }
 
-def subring.inter [S₁ : subring R] [S₂ : subring R] (S₁ ∩ S₂) :
-  subring R := 
-{ carrier := (S₁ ∩ S₂).carrier,
-..(S₁ ∩ S₂).subset_comm_ring }
+instance : has_bot (subring R) := ⟨(int.cast_ring_hom R).set_range⟩
 
-instance subring.Inter {ι : Sort*} (S : ι → subring R) /-[h : ∀ y : ι, is_subring (S y)]-/ :
-  subring R :=
-{carrier := (set.Inter S) }
+instance : inhabited (subring R) := ⟨⊥⟩
 
-lemma is_subring_Union_of_directed {ι : Type*} [hι : nonempty ι]
-  (s : ι → subring R) [∀ i : ι, (s i) : subring R ]
+lemma coe_bot : ((⊥ : subring R) : set R) = set.range (coe : ℤ → R) := ring_hom.coe_set_range(int.cast_ring_hom R)
+
+lemma mem_bot {x : R} : x ∈ (⊥ : subring R) ↔ ∃ (n : ℤ), ↑n = x :=  ring_hom.mem_set_range
+
+/-- The inf of two subrings is their intersection. -/
+instance : has_inf (subring R) :=
+⟨λ s t,
+  { carrier := s ∩ t,
+    .. s.to_submonoid ⊓ t.to_submonoid,
+    .. s.to_add_subgroup ⊓ t.to_add_subgroup }⟩
+
+instance : has_inter (subring R) := ⟨λ U V, U ⊓ V⟩ 
+
+@[simp] lemma coe_inf (p p' : subring R) : ((p ⊓ p' : subring R) : set R) = p ∩ p' := rfl
+
+@[simp] lemma mem_inf {p p' : subring R} {x : R} : x ∈ p ⊓ p' ↔ x ∈ p ∧ x ∈ p' := iff.rfl
+
+instance : has_Inf (subring R) := 
+⟨λ s, subring.mk' (⋂ t ∈ s, ↑t) (⨅ t ∈ s, subring.to_submonoid t) (by simp)
+  (⨅ t ∈ s, subring.to_add_subgroup t) ( begin simp only [<-add_subgroup.has_Inf, <-add_subgroup.coe_to_submonoid, add_submonoid.coe_infi], rw <-add_submonoid.coe_infi, end)⟩ 
+
+@[simp, norm_cast] lemma coe_Inf (S : set (subring R)) :
+  ((Inf S : subring R) : set R) = ⋂ s ∈ S, ↑s := rfl
+
+lemma mem_Inf {S : set (subring R)} {x : R} : x ∈ Inf S ↔ ∀ p ∈ S, x ∈ p := set.mem_bInter_iff
+
+@[simp] lemma Inf_to_submonoid (s : set (subring R)) :
+  (Inf s).to_submonoid = ⨅ t ∈ s, subring.to_submonoid t :=
+mk'_to_submonoid _ _
+
+@[simp] lemma Inf_to_add_submonoid (s : set (subring R)) :
+  (Inf s).to_add_subgroup = ⨅ t ∈ s, subring.to_add_subgroup t :=
+mk'_to_add_subgroup _ _
+
+/-- Subrings of a ring form a complete lattice. -/
+instance : complete_lattice (subring R) :=
+{ bot := (⊥),
+  bot_le := λ s x hx, let ⟨n, hn⟩ := mem_bot.1 hx in hn ▸ s.coe_int_mem n,
+  top := (⊤),
+  le_top := λ s x hx, trivial,
+  inf := (⊓),
+  inf_le_left := λ s t x, and.left,
+  inf_le_right := λ s t x, and.right,
+  le_inf := λ s t₁ t₂ h₁ h₂ x hx, ⟨h₁ hx, h₂ hx⟩,
+  .. complete_lattice_of_Inf (subring R)
+    (λ s, is_glb.of_image (λ s t, show (s : set R) ≤ t ↔ s ≤ t, from coe_subset_coe) is_glb_binfi)}
+
+--is this needed?
+def subring.inter [S₁ : subring R] [S₂ : subring R] :
+  subring R := (S₁ ∩ S₂)
+
+--invalid
+/- instance subring.Inter {ι : Sort*} (S : ι → subring R) : subring R := set.Inter S 
+
+lemma subring_Union_of_directed {ι : Type*} [hι : nonempty ι] (s : ι → subring R)  
   (directed : ∀ i j, ∃ k, s i ⊆ s k ∧ s j ⊆ s k) :
   subring R :=
 { carrier := (⋃i, s i)
-  to_is_add_subgroup := is_add_subgroup_Union_of_directed s directed,
-  to_is_submonoid := is_submonoid_Union_of_directed s directed }
+  to_add_subgroup := add_subgroup_Union_of_directed s directed,
+  to_submonoid := submonoid_Union_of_directed s directed }
+-/
+
+/-- The `subring` generated by a set. -/
+def closure (s : set R) : subring R := Inf {S | s ⊆ S}
+
+lemma mem_closure {x : R} {s : set R} : x ∈ closure s ↔ ∀ S : subring R, s ⊆ S → x ∈ S :=
+mem_Inf
+
+/-- The subring generated by a set includes the set. -/
+@[simp] lemma subset_closure {s : set R} : s ⊆ closure s := λ x hx, mem_closure.2 $ λ S hS, hS hx
+
+/-- A subring `S` includes `closure s` if and only if it includes `s`. -/
+@[simp]
+lemma closure_le {s : set R} {t : subring R} : closure s ≤ t ↔ s ⊆ t :=
+⟨set.subset.trans subset_closure, λ h, Inf_le h⟩
+
+/-- Subring closure of a set is monotone in its argument: if `s ⊆ t`,
+then `closure s ≤ closure t`. -/
+lemma closure_mono ⦃s t : set R⦄ (h : s ⊆ t) : closure s ≤ closure t :=
+closure_le.2 $ set.subset.trans h subset_closure
+
+lemma closure_eq_of_le {s : set R} {t : subring R} (h₁ : s ⊆ t) (h₂ : t ≤ closure s) :
+  closure s = t :=
+le_antisymm (closure_le.2 h₁) h₂
+
+/-- An induction principle for closure membership. If `p` holds for `0`, `1`, and all elements
+of `s`, and is preserved under addition and multiplication, then `p` holds for all elements
+of the closure of `s`. -/
+-- @[elab_as_eliminator]
+lemma closure_induction {s : set R} {p : R → Prop} {x} (h : x ∈ closure s)
+  (Hs : ∀ x ∈ s, p x) (H0 : p 0) (H1 : p 1)
+  (Hadd : ∀ x y, p x → p y → p (x + y)) (Hneg : ∀ (x : R), p x → p (-x)) (Hmul : ∀ x y, p x → p y → p (x * y)) : p x :=
+(@closure_le _ _ _ ⟨p, H1, Hmul, H0, Hadd, Hneg⟩).2 Hs h
+
+-- set_option pp.all true
+lemma mem_closure_iff {s : set R} {x} :
+  x ∈ closure s ↔ x ∈ add_subgroup.closure (submonoid.closure s : set R) :=
+⟨λ h, closure_induction h (λ x hx, add_subgroup.subset_closure $ submonoid.subset_closure hx)
+  (add_subgroup.zero_mem _)
+  (add_subgroup.subset_closure $ submonoid.one_mem $ submonoid.closure s)
+  (λ _ _, add_subgroup.add_mem _)
+  (λ _, add_subgroup.neg_mem _)
+  (λ x y ihx ihy, add_subgroup.closure_induction ihy
+    (λ q hq, add_subgroup.closure_induction ihx
+      (λ p hp, add_subgroup.subset_closure $ (submonoid.closure s).mul_mem hp hq)
+      ((zero_mul q).symm ▸ add_subgroup.zero_mem _)
+      (λ p₁ p₂ ihp₁ ihp₂, (add_mul p₁ p₂ q).symm ▸ add_subgroup.add_mem _ ihp₁ ihp₂))
+    ((mul_zero x).symm ▸ add_subgroup.zero_mem _)
+    (λ q₁ q₂ ihq₁ ihq₂, (mul_add x q₁ q₂).symm ▸ add_subgroup.add_mem _ ihq₁ ihq₂)),
+λ h, add_subgroup.closure_induction h
+  (λ x hx, submonoid.closure_induction hx subset_closure (one_mem _) (λ _ _, mul_mem _))
+  (zero_mem _)
+  (λ _ _, add_mem _)
+  (λ _, neg_mem _)⟩
+
+lemma mem_closure_iff_exists_list {s : set R} {x} : x ∈ closure s ↔
+  ∃ L : list (list R), (∀ t ∈ L, ∀ y ∈ t, y ∈ s) ∧ (L.map list.prod).sum = x :=
+⟨λ hx, add_subgroup.closure_induction (mem_closure_iff.1 hx)
+  (λ x hx, suffices ∃ t : list R, (∀ y ∈ t, y ∈ s) ∧ t.prod = x,
+    from let ⟨t, ht1, ht2⟩ := this in ⟨[t], list.forall_mem_singleton.2 ht1,
+      by rw [list.map_singleton, list.sum_singleton, ht2]⟩,
+    submonoid.closure_induction hx
+      (λ x hx, ⟨[x], list.forall_mem_singleton.2 hx, one_mul x⟩)
+      ⟨[], list.forall_mem_nil _, rfl⟩
+      (λ x y ⟨t, ht1, ht2⟩ ⟨u, hu1, hu2⟩, ⟨t ++ u, list.forall_mem_append.2 ⟨ht1, hu1⟩,
+        by rw [list.prod_append, ht2, hu2]⟩))
+  ⟨[], list.forall_mem_nil _, rfl⟩
+  (λ x y ⟨L, HL1, HL2⟩ ⟨M, HM1, HM2⟩, ⟨L ++ M, list.forall_mem_append.2 ⟨HL1, HM1⟩,
+    by rw [list.map_append, list.sum_append, HL2, HM2]⟩),
+λ ⟨L, HL1, HL2⟩, HL2 ▸ list_sum_mem _ (λ r hr, let ⟨t, ht1, ht2⟩ := list.mem_map.1 hr in
+  ht2 ▸ list_prod_mem _ (λ y hy, subset_closure $ HL1 t ht1 y hy))⟩
+#exit
+
+variable (R)
+
+/-- `closure` forms a Galois insertion with the coercion to set. -/
+protected def gi : galois_insertion (@closure R _) coe :=
+{ choice := λ s _, closure s,
+  gc := λ s t, closure_le,
+  le_l_u := λ s, subset_closure,
+  choice_eq := λ s h, rfl }
+
+variable {R}
+
+/-- Closure of a subsemiring `S` equals `S`. -/
+lemma closure_eq (s : subsemiring R) : closure (s : set R) = s := (subsemiring.gi R).l_u_eq s
+
+@[simp] lemma closure_empty : closure (∅ : set R) = ⊥ := (subsemiring.gi R).gc.l_bot
+
+@[simp] lemma closure_univ : closure (set.univ : set R) = ⊤ := @coe_top R _ ▸ closure_eq ⊤
+
+lemma closure_union (s t : set R) : closure (s ∪ t) = closure s ⊔ closure t :=
+(subsemiring.gi R).gc.l_sup
+
+lemma closure_Union {ι} (s : ι → set R) : closure (⋃ i, s i) = ⨆ i, closure (s i) :=
+(subsemiring.gi R).gc.l_supr
+
+lemma closure_sUnion (s : set (set R)) : closure (⋃₀ s) = ⨆ t ∈ s, closure t :=
+(subsemiring.gi R).gc.l_Sup
+
+--till 403
 
 namespace ring
 
 def closure (s : set R) := add_group.closure (monoid.closure s)
+
 variable {s : set R}
 local attribute [reducible] closure
 
